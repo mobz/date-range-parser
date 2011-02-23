ebc29c3442b794a3cc484a9f8907362da461d22f
mobz <bbirch@aconex.com>
2011-02-23 23:34:28 +1100

    support for now and bare hour

--- src/date-range-parser.js
+++ src/date-range-parser.js
@@ -1,16 +1,51 @@
 (function() {
 
 	var drp = window.dateRangeParser = {};
+
 	drp.defaultRange = 1000 * 60 * 60 * 24;
+
 	drp.now = null; // set a different value for now than the time at function invocation
+
 	drp.parse = function parse(v) {
 		var now = this.now || new Date().getTime();
-		var r = { start: 0, end: 0 };
-		console.log(v, v.split(/([^<>]+)?(<>?)?([^<>]+)?/))
-		if(v === "now") {
-			r.start = now - (this.defaultRange/2);
-			r.end = now + (this.defaultRange/2);
+
+		function makeArray(d) {
+			var da = new Date(d);
+			return [ da.getUTCFullYear(), da.getUTCMonth(), da.getUTCDate(), da.getUTCHours(), da.getUTCMinutes(), da.getUTCSeconds(), da.getUTCMilliseconds() ];
 		}
-		return r;
+		function fromArray(a) {
+			return Date.UTC.apply(null, a);
+		}
+		function zeroArray(d, p, v) {
+			var da = makeArray(d);
+			da[p] = v;
+			for(var i = p+1; i < da.length; i++) {
+				da[i] = i === 1 ? -1 : 0;
+			}
+			return da;
+		}
+		function procTerm(term) {
+			var r = { type: "d", start: 0, end: 0 };
+			if(term === "now") {
+				r.start = now - drp.defaultRange;
+				r.end = now + drp.defaultRange;
+			} if(term.match(/^\d{1,2}$/)) {
+				var da = zeroArray(now, 3, parseInt(term, 10));
+				r.start = fromArray(da);
+				da[3]++;
+				r.end = fromArray(da);
+			}
+			return r;
+		}
+
+		var terms = v.split(/\s*([^<>\s]+)?\s*(<>?)?\s*([^<>\s]+)?\s*/);
+		console.log(v, terms)
+		if(terms[1] !== "") {
+			op1 = procTerm(terms[1]);
+		}
+		if(terms[3] !== "") {
+			op2 = procTerm(terms[3]);
+		}
+		return op1;
 	}
 })();
--- test/date-range-parser-test.js
+++ test/date-range-parser-test.js
@@ -7,14 +7,15 @@ function same(s, e, exp) {
 
 DateRangeParserTest.prototype.testParse = function() {
 	var now = 1000000000000;
+	var nowSD = 999993600000;
 	var sec = 1000;
 	var min = sec * 60;
 	var hr = min * 60;
 	var day = hr * 24;
 	var drp = window.dateRangeParser;
 	drp.now = 1000 * 1000 * 1000 * 1000;
-	same(999956800000, 1000043200000, drp.parse("now"));
-	same(now, now + hr, drp.parse("0"));
-	same(now + hr, now + 2*hr, drp.parse("1"));
-	same(now + 23*hr, now + 24*hr, drp.parse("23"));
+	same(now - day, now + day, drp.parse("now"));
+	same(nowSD, nowSD + hr, drp.parse("0"));
+	same(nowSD + hr, nowSD + 2*hr, drp.parse("1"));
+	same(nowSD + 23*hr, nowSD + 24*hr, drp.parse("23"));
 };
