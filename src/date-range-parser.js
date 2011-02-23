6dd0599713f3f5b93f509c21275359bc320c4da5
mobz <bbirch@aconex.com>
2011-02-23 16:52:58 +1100

    initial commit of lib files

---
+++ src/date-range-parser.js
@@ -0,0 +1,15 @@
+(function() {
+
+	var drp = window.dateRangeParser = {};
+	drp.defaultRange = 1000 * 60 * 60 * 24;
+	drp.now = null; // set a different value for now than the time at function invocation
+	drp.parse = function parse(v) {
+		var now = this.now || new Date().getTime();
+		var r = { start: 0, end: 0 };
+		if(v === "now") {
+			r.start = now - (this.defaultRange/2);
+			r.end = now + (this.defaultRange/2);
+		}
+		return ret;
+	}
+})();
